34ecd43d3fdff2b27548e31388e9ea130ef940d0
杨振龙 <yangzhenlong@yangzhenlongdeMacBook-Pro.local>
2020-07-07 10:49:16 +0800

    mongoDB配置

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,24 +1,26 @@
 const mongoose = require('mongoose')
 
-const DB_URL = 'mongodb://39.99.211.142:27017/zch_user'
+const { mongoDB } = require('./config.json')
+
+const DB_URL = mongoDB
 
 mongoose.set('useCreateIndex', true)
 mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
  
 mongoose.connection.on('connected',function() {
-  //  console.log('Mongoose connection open to '+DB_URL);
+   console.log('数据库已连接');
 });
 /**
 * 连接异常 error 数据库连接错误
 */
 mongoose.connection.on('error',function(err) {
-  console.log('Mongoose connection error: '+ err);
+  console.log('数据库连接错误: '+ err);
 });
 /**
 * 连接断开 disconnected 连接异常断开
 */
 mongoose.connection.on('disconnected',function() {
-  console.log('Mongoose connection disconnected');
+  console.log('数据库已断开');
 })
 
 module.exports = mongoose
